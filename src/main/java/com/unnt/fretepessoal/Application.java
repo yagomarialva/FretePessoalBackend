4f3622d996c5ac4245d7fbe88c0e3f20cece41ee
yagomarialva <yago.marialva@gmail.com>
2022-06-13 19:03:51 -0400

    Adding swagger without property security

--- src/main/java/com/unnt/fretepessoal/Application.java
+++ src/main/java/com/unnt/fretepessoal/Application.java
@@ -2,8 +2,17 @@ package com.unnt.fretepessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "frete-pessoal-api", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(info = @Info(title = "Frete Pessoal API", version = "1.0", description = "User Details"))
 public class Application {
 
 	public static void main(String[] args) {
